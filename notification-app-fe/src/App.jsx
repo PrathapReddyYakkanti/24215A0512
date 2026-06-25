23f2b7e01a3dd7b7e007ca73800251c03f1f2ebd
PrathapReddy <yprathapreddy63015@gmail.com>
2026-06-25 15:15:06 +0530

    completed

--- notification-app-fe/src/App.jsx
+++ notification-app-fe/src/App.jsx
@@ -1,3 +1,106 @@
+import { useEffect, useState } from "react";
+import { fetchNotifications } from "./api/notifications";
+
 export default function App() {
-  return "Notifications App";
+  const [notifications, setNotifications] = useState([]);
+  const [selectedType, setSelectedType] = useState("All");
+  const [search, setSearch] = useState("");
+  const [sortOrder, setSortOrder] = useState("Latest");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadNotifications();
+  }, []);
+
+  async function loadNotifications() {
+    setLoading(true);
+
+    try {
+      const data = await fetchNotifications();
+      setNotifications(data.notifications);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  }
+
+  const filteredNotifications = notifications
+    .filter((notification) => {
+      const typeMatch =
+        selectedType === "All" || notification.Type === selectedType;
+
+      const searchMatch = notification.Message.toLowerCase().includes(
+        search.toLowerCase(),
+      );
+
+      return typeMatch && searchMatch;
+    })
+    .sort((a, b) => {
+      if (sortOrder === "Latest") {
+        return new Date(b.Timestamp) - new Date(a.Timestamp);
+      } else {
+        return new Date(a.Timestamp) - new Date(b.Timestamp);
+      }
+    });
+
+  return (
+    <div>
+      <h1>Notifications App</h1>
+
+      <button onClick={loadNotifications}>Refresh</button>
+
+      <p>Total Notifications: {filteredNotifications.length}</p>
+
+      <input
+        type="text"
+        placeholder="Search by message"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <select
+        value={selectedType}
+        onChange={(e) => setSelectedType(e.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="Placement">Placement</option>
+        <option value="Result">Result</option>
+        <option value="Event">Event</option>
+      </select>
+
+      <select
+        value={sortOrder}
+        onChange={(e) => setSortOrder(e.target.value)}
+        style={{ marginLeft: "10px" }}
+      >
+        <option value="Latest">Latest First</option>
+        <option value="Oldest">Oldest First</option>
+      </select>
+
+      <hr />
+
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        filteredNotifications.map((notification) => (
+          <div key={notification.ID}>
+            <p>
+              <b>Type:</b> {notification.Type}
+            </p>
+            <p>
+              <b>Message:</b> {notification.Message}
+            </p>
+            <p>
+              <b>Time:</b> {new Date(notification.Timestamp).toLocaleString()}
+            </p>
+            <hr />
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,36 @@
-export function fetchNotifications() {
+export async function fetchNotifications() {
+  // Login
+  const authResponse = await fetch(
+    "http://4.224.186.213/evaluation-service/auth",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    body: JSON.stringify({
+  email: "24215a0512@gmail.com",
+  name: "prathap reddy yakkanti",
+  rollNo: "24215a512",
+  accessCode: "ahXjvp",
+  clientID: "5e52fa45-8b67-469f-8228-e707b78e2314",
+  clientSecret: "nESjnfuMvChMaugE",
+}),
+    }
+  );
+
+  const authData = await authResponse.json();
+
+ console.log(authData);
+
+  // Fetch notifications
+  const response = await fetch(
+    "http://4.224.186.213/evaluation-service/notifications",
+    {
+      headers: {
+        Authorization: `${authData.token_type} ${authData.access_token}`,
+      },
+    }
+  );
+
+  return await response.json();
 }
